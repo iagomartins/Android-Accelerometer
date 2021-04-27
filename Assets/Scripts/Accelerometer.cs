53a973816ff22ac6dd823431f2ddb61130a8ccb5
Iago Martins <iagomartinsgamedesign@gmail.com>
2021-04-27 14:55:07 -0300

    Accelerometer Baloon
    Move the baloon according to device world rotation.

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof (Rigidbody))]
+public class Accelerometer : MonoBehaviour
+{
+    public bool isFlat = false;
+    public float speed;
+    public Rigidbody rb;
+    public string sceneName;
+    private Vector3 force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateForceBasedOnAccelerometer();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    /// <summary>
+    /// Calculate player movements after getting the device rotation.
+    /// </summary>
+    private void CalculateForceBasedOnAccelerometer() {
+        Vector3 _v = Input.acceleration;
+        if(isFlat) {
+            _v = Quaternion.Euler(90, 0, 0) * _v;
+        }
+        force = _v * speed * Time.deltaTime;
+    }
+
+    private void Movement() {
+        rb.velocity = new Vector3(force.x, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "obstacle") {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameController : MonoBehaviour
+{
+    public float playerHeight;
+    public float speed;
+    public Transform[] spawnPoints;
+    public Transform center;
+    public GameObject obstaclePrefab;
+    public float timeBetweenSpawns;
+    public Text playerHeightText;
+    private float counter;
+    private int maxWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxWidth = spawnPoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Atribui valor a variável de altura do jogador.
+        playerHeight = transform.position.y;
+        playerHeightText.text = "Altura alcançada: " + Math.Round(playerHeight, 1) + " metros.";
+        SpawnObstacle();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(0, speed * Time.fixedDeltaTime, 0);
+    }
+
+    void SpawnObstacle() {
+        if(counter < timeBetweenSpawns) {
+            counter += Time.deltaTime;
+        }
+        else {
+            GameObject obstacle = Instantiate(obstaclePrefab, spawnPoints[SortSpawnPoint()].position, Quaternion.identity, center);
+            counter = 0;
+        }
+    }
+
+    /// <summary>
+    /// Randomiza uma posiçao entre o vetor de transforms.
+    /// </summary>
+    /// <returns>Returns a random integer between 0 and the number of positions inside the spawn points array.</returns>
+    int SortSpawnPoint() {
+        int _ret = UnityEngine.Random.Range(0, maxWidth);
+        return _ret;
+    }
+}
